6ea145c9d24562a4f235c7a82e441542aaf702e6
paras603 <parasmanirai1@gmail.com>
2023-03-28 21:31:54 +0545

    adapter created

---
+++ app/src/main/java/com/example/to_do/Todo.kt
@@ -0,0 +1,6 @@
+package com.example.to_do
+
+data class Todo(
+    val title: String,
+    val isChecked: Boolean = false
+)
---
+++ app/src/main/java/com/example/to_do/TodoAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.to_do
+
+class TodoAdapter(
+    private val todos: MutableList<Todo>
+){
+
+}
