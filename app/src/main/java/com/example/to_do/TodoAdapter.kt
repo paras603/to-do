ec95d4b73bbcfef8c012b43dfc24e2b3dede6347
paras603 <parasmanirai1@gmail.com>
2023-03-29 22:24:05 +0545

    todo adapter?

--- app/src/main/java/com/example/to_do/TodoAdapter.kt
+++ app/src/main/java/com/example/to_do/TodoAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.to_do
 
+import androidx.recyclerview.widget.RecyclerView
+
 class TodoAdapter(
     private val todos: MutableList<Todo>
-){
-
+) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>(){
+    class TodoViewHolder (itemView: View) : RecyclerView.ViewHolder (itemView)
 }
