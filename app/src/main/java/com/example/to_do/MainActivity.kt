aaffb5bfa73f115e2224d0d1bd58c3a17185e182
paras603 <parasmanirai1@gmail.com>
2023-04-05 21:04:02 +0545

    add and delete function added

--- app/src/main/java/com/example/to_do/MainActivity.kt
+++ app/src/main/java/com/example/to_do/MainActivity.kt
@@ -4,8 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvRodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty){
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTile.text.clear()
+            }
+        }
+        btnDeleteDoneTodos.setOnClickListerner {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
--- app/src/main/java/com/example/to_do/Todo.kt
+++ app/src/main/java/com/example/to_do/Todo.kt
@@ -2,5 +2,5 @@ package com.example.to_do
 
 data class Todo(
     val title: String,
-    val isChecked: Boolean = false
+    var isChecked: Boolean = false
 )
